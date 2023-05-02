abb8cb7d30eac266448bb4c1d61521b9f939f3b9
Yehun Kim <98015104+gnsdp99@users.noreply.github.com>
2023-05-03 00:43:55 +0900

    Update 23971.py

--- baekjoon/23971.py
+++ baekjoon/23971.py
@@ -1,3 +1,4 @@
+# 풀이 1.
 import sys
 input = sys.stdin.readline
 
@@ -13,3 +14,13 @@ for _ in range(0, H, N+1):
     
 ans = r * c
 print(ans)
+
+# 풀이 2.
+# import sys
+# input = sys.stdin.readline
+# h, w, n, m = map(int, input().split())
+
+# col = (h-1) // (n+1) + 1
+# row = (w-1) // (m+1) + 1
+
+# print(row*col)
