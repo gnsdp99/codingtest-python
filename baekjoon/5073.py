f945a1c1ac1f9f37080e406c301d5b41699f5a9d
gnsdp99 <rladpgns99@gmail.com>
2023-05-03 15:53:26 +0900

    Update code

--- baekjoon/5073.py
+++ baekjoon/5073.py
@@ -3,14 +3,16 @@ input = sys.stdin.readline
 
 while True:
     s1, s2, s3 = map(int, input().split()) # s: the length of side
-    if not all([s1, s2, s3]):
+    if all(s == 0 for s in [s1, s2, s3]):
         break
-    
+     
     if s1 == s2 == s3:
-        print("Equilateral")
+        result = "Equilateral"
     elif (s1 >= s2+s3) or (s2 >= s1+s3) or (s3 >= s1+s2):
-        print("Invalid")
+        result = "Invalid"
     elif s1 == s2 or s2 == s3 or s3 == s1:
-        print("Isosceles")
+        result = "Isosceles"
     else:
-        print("Scalene")
+        result = "Scalene"
+        
+    print(result)
