afa82a368097c1fd0d7f93b59b3c6b8b2fde8174
gnsdp99 <rladpgns99@gmail.com>
2023-05-05 20:23:37 +0900

    Update 1157

--- baekjoon/1157.py
+++ baekjoon/1157.py
@@ -11,7 +11,6 @@ if len(numAlpha) >= 2 and numAlpha[0][1] == numAlpha[1][1]:
 else: 
     print(numAlpha[0][0])
 
-
 # 풀이 2
 import sys
 from collections import Counter
@@ -22,3 +21,12 @@ numAlpha = Counter(word)
 
 maxNumAlpha = [k for k, v in numAlpha.items() if max(numAlpha.values()) == v]
 print("?") if len(maxNumAlpha) >= 2 else print(maxNumAlpha[0])
+
+# 풀이 3
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+word = input().rstrip().upper() # remove '\n'
+numAlpha = Counter(word).most_common(2)
+print("?") if len(numAlpha) >= 2 and numAlpha[0][1] == numAlpha[1][1] else print(numAlpha[0][0])
