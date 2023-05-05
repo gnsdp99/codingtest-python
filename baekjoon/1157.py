700335b3f43a1386232ec021013c6d37f6219af3
gnsdp99 <rladpgns99@gmail.com>
2023-05-05 12:14:45 +0900

    1157 단어 공부

---
+++ baekjoon/1157.py
@@ -0,0 +1,24 @@
+# 풀이 1
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+word = input().rstrip().upper() # remove '\n'
+numAlpha = sorted(Counter(word).items(), key=lambda x:x[1], reverse=True)
+
+if len(numAlpha) >= 2 and numAlpha[0][1] == numAlpha[1][1]:
+    print("?")
+else: 
+    print(numAlpha[0][0])
+
+
+# 풀이 2
+import sys
+from collections import Counter
+input = sys.stdin.readline
+
+word = input().rstrip().upper() # remove '\n'
+numAlpha = Counter(word)
+
+maxNumAlpha = [k for k, v in numAlpha.items() if max(numAlpha.values()) == v]
+print("?") if len(maxNumAlpha) >= 2 else print(maxNumAlpha[0])
